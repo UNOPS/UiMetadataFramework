a7dcd0f7328f92e898b74c4ef98ebb0163146dc3
Mohammed Fuad Salem <mohammed.fuad@gmail.com>
2017-07-16 15:53:37 +0300

    setup vue.js client

---
+++ clients/vue-js/src/components/Hello.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'UIMetadataFramework client in Vue.js'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
