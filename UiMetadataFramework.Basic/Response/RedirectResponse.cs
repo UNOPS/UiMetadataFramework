8bcc052623c685c7d4d207a9c6a779c82e0a8ddd
Nik P <niaher.crimston@gmail.com>
2017-07-25 07:59:41 +0300

    release

--- UiMetadataFramework.Basic/Response/RedirectResponse.cs
+++ UiMetadataFramework.Basic/Response/RedirectResponse.cs
@@ -1,5 +1,6 @@
 namespace UiMetadataFramework.Basic.Response
 {
+	using System.Collections.Generic;
 	using UiMetadataFramework.Core;
 
 	public class RedirectResponse : FormResponse
@@ -8,6 +9,14 @@ namespace UiMetadataFramework.Basic.Response
 		{
 		}
 
+		/// <summary>
+		/// Gets or sets name of the form to redirect to.
+		/// </summary>
 		public string Form { get; set; }
+
+		/// <summary>
+		/// Gets or sets values for the input fields of the form (i.e. - <see cref="FormMetadata.InputFields"/>).
+		/// </summary>
+		public IDictionary<string, object> InputFieldValues { get; set; }
 	}
 }
