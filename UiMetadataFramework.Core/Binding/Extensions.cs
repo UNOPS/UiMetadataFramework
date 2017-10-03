198958412b40b4a51dbfbec267f7b9ac60ecd424
Nik P <niaher.crimston@gmail.com>
2017-10-03 21:05:48 +0300

    fix(core): NotField was not being respected in netstandard2.0

--- UiMetadataFramework.Core/Binding/Extensions.cs
+++ UiMetadataFramework.Core/Binding/Extensions.cs
@@ -15,22 +15,12 @@
 			}
 		}
 
-		public static IEnumerable<PropertyInfo> GetFields(this Type type)
-		{
-			return type.GetRuntimeProperties()
-				.Where(t => t.GetCustomAttribute<NotFieldAttribute>() == null)
-				.ToList();
-		}
-
 		public static IEnumerable<PropertyInfo> GetPublicProperties(this Type type)
 		{
-#if NETSTANDARD1_6
-			return type.GetFields();
-#endif
-
-#if NETSTANDARD2_0
-			return type.GetProperties();
-#endif
+			return type.GetRuntimeProperties()
+				.Where(t => t.CanRead && t.GetMethod.IsPublic)
+				.Where(t => t.GetCustomAttribute<NotFieldAttribute>() == null)
+				.ToList();
 		}
 	}
 }
