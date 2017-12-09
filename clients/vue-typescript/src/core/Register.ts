6768778e13cc498eedbe4109b7c25b67a04f41fd
Mohammed Salem <mohammed.fuad@gmail.com>
2017-12-09 15:22:59 +0100

    cleanup

--- clients/vue-typescript/src/core/Register.ts
+++ clients/vue-typescript/src/core/Register.ts
@@ -4,18 +4,19 @@ import * as handlers from "core-handlers";
 import controlRegister from "./ControlRegister";
 import { SystemRouter } from './SystemRouter';
 
-
 export class Register {
 	private readonly server: any;
 	public readonly app: any;
+	private readonly router: any;
 
 	constructor(router: any) {
 		this.server = new UmfServer("http://localhost:62790/api/form/metadata", "http://localhost:62790/api/form/run");
 		this.app = new UmfApp(this.server, controlRegister);
+		this.router = router;
 
 		this.app.load().then(response => {
-			var router = new SystemRouter(router, this.app);
-			this.app.useRouter(router);
+			var _router = new SystemRouter(this.router, this.app);
+			this.app.useRouter(_router);
 
 			this.app.registerResponseHandler(new handlers.MessageResponseHandler());
 			this.app.registerResponseHandler(new handlers.ReloadResponseHandler((form, inputFieldValues) => {
