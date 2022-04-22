d3e2095ef74db7f506c74566823aab60058acb00
MohammedT <honeyize14@hotmail.com>
2022-04-23 01:46:59 +0300

    upgrade to .net6

--- UiMetadataFramework.Web/Program.cs
+++ UiMetadataFramework.Web/Program.cs
@@ -1,21 +1,43 @@
 ﻿namespace UiMetadataFramework.Web
 {
-	using System.IO;
-	using Microsoft.AspNetCore.Hosting;
+    using System.IO;
+    using Lamar.Microsoft.DependencyInjection;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
 
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var host = new WebHostBuilder()
-				.UseKestrel()
-				.UseContentRoot(Directory.GetCurrentDirectory())
-				.UseIISIntegration()
-				.UseStartup<Startup>()
-				.UseApplicationInsights()
-				.Build();
+    public class Program
+    {
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseLamar()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .UseKestrel()
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseIISIntegration()
+                        .UseStartup<Startup>();
+                    webBuilder.ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.AddConsole();
+                        logging.AddDebug();
+                    });
 
-			host.Run();
-		}
-	}
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        // This is important, the call to AddControllers()
+                        // cannot be made before the usage of ConfigureWebHostDefaults
+                        services.AddControllers();
+                        services.AddApplicationInsightsTelemetry();
+                    });
+                });
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+    }
 }
--- UiMetadataFramework.Web/Startup.cs
+++ UiMetadataFramework.Web/Startup.cs
@@ -1,111 +1,107 @@
 ﻿namespace UiMetadataFramework.Web
 {
-	using System;
-	using global::MediatR;
-	using Microsoft.AspNetCore.Builder;
-	using Microsoft.AspNetCore.Hosting;
-	using Microsoft.Extensions.Configuration;
-	using Microsoft.Extensions.DependencyInjection;
-	using Microsoft.Extensions.Logging;
-	using StructureMap;
-	using StructureMap.TypeRules;
-	using UiMetadataFramework.Basic.Input;
-	using UiMetadataFramework.Core.Binding;
-	using UiMetadataFramework.MediatR;
-	using UiMetadataFramework.Web.Forms;
-	using UiMetadataFramework.Web.Middleware;
+    using global::MediatR;
+    using Lamar;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using UiMetadataFramework.Basic.Input;
+    using UiMetadataFramework.Core.Binding;
+    using UiMetadataFramework.MediatR;
+    using UiMetadataFramework.Web.Forms;
+    using UiMetadataFramework.Web.Middleware;
 
-	public class Startup
-	{
-		public const string CorsAllowAllPolicy = "AllowAll";
+    public class Startup
+    {
+        public const string CorsAllowAllPolicy = "AllowAll";
 
-		public Startup(IHostingEnvironment env)
-		{
-			var builder = new ConfigurationBuilder()
-				.SetBasePath(env.ContentRootPath)
-				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-				.AddEnvironmentVariables();
-			this.Configuration = builder.Build();
-		}
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            this.Configuration = builder.Build();
+        }
 
-		public IConfigurationRoot Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-		{
-			loggerFactory.AddConsole(this.Configuration.GetSection("Logging"));
-			loggerFactory.AddDebug();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+            app.UseMiddleware(typeof(ErrorHandlingMiddleware));
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
-			app.UseMiddleware(typeof(ErrorHandlingMiddleware));
-			app.UseDefaultFiles();
-			app.UseStaticFiles();
+            app.UseRouting();
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
 
-			app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-			app.UseMvc();
-		}
+        public void ConfigureContainer(ServiceRegistry registry)
+        {
+            registry.For<MetadataBinder>().Use(GetMetadataBinder).Singleton();
+            registry.For<FormRegister>().Use(GetFormRegister).Singleton();
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public IServiceProvider ConfigureServices(IServiceCollection services)
-		{
-			services.AddCors(o => o.AddPolicy(CorsAllowAllPolicy, builder =>
-			{
-				builder.AllowAnyOrigin()
-					.AllowAnyMethod()
-					.AllowAnyHeader();
-			}));
+            registry.Scan(_ =>
+            {
+                _.AssembliesFromApplicationBaseDirectory();
+                _.WithDefaultConventions();
+            });
+        }
 
-			// Add framework services.
-			services.AddMvc().AddJsonOptions(options =>
-			{
-				options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
-				options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Include;
-			});
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(o => o.AddPolicy(CorsAllowAllPolicy, builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
 
-			services.AddMediatR(typeof(SearchPeople));
-			services.AddMediatR(typeof(InvokeForm));
+            // Add framework services.
+            services.AddMvc(
+                    options => { options.EnableEndpointRouting = false; })
+                .AddNewtonsoftJson(
+                    options =>
+                    {
+                        options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                        options.SerializerSettings.NullValueHandling = NullValueHandling.Include;
+                    });
 
-			var container = new Container();
-			container.Configure(config =>
-			{
-				config.For<MetadataBinder>().Use(t => GetMetadataBinder(t)).Singleton();
-				config.For<FormRegister>().Use(t => GetFormRegister(t)).Singleton();
+            services.AddMediatR(typeof(SearchPeople));
+            services.AddMediatR(typeof(InvokeForm));
+        }
 
-				config.Scan(_ =>
-				{
-					_.AssembliesFromApplicationBaseDirectory();
-					_.WithDefaultConventions();
-				});
-			});
+        private static FormRegister GetFormRegister(IServiceContext context)
+        {
+            var register = new FormRegister(context.GetInstance<MetadataBinder>());
+            register.RegisterAssembly(typeof(SearchPeople).Assembly);
+            return register;
+        }
 
-			// Populate the container using the service collection.
-			// This will register all services from the collection
-			// into the container with the appropriate lifetime.
-			container.Populate(services);
-
-			// Finally, make sure we return an IServiceProvider. This makes
-			// ASP.NET use the StructureMap container to resolve its services.
-			return container.GetInstance<IServiceProvider>();
-		}
-
-		private static FormRegister GetFormRegister(IContext context)
-		{
-			var register = new FormRegister(context.GetInstance<MetadataBinder>());
-			register.RegisterAssembly(typeof(SearchPeople).GetAssembly());
-			return register;
-		}
-
-		private static MetadataBinder GetMetadataBinder(IContext context)
-		{
-			var binder = new MetadataBinder(new DependencyInjectionContainer(context.GetInstance));
-			binder.RegisterAssembly(typeof(StringInputFieldBinding).GetAssembly());
-			binder.RegisterAssembly(typeof(SearchPeople).GetAssembly());
-			return binder;
-		}
-	}
+        private static MetadataBinder GetMetadataBinder(IServiceContext context)
+        {
+            var binder = new MetadataBinder(new DependencyInjectionContainer(context.GetInstance));
+            binder.RegisterAssembly(typeof(StringInputFieldBinding).Assembly);
+            binder.RegisterAssembly(typeof(SearchPeople).Assembly);
+            return binder;
+        }
+    }
 }
