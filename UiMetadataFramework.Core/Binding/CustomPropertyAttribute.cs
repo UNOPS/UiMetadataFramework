202700f7f304b48dea8f3dc46d7beecc152c5818
Nik P <niaher.crimston@gmail.com>
2024-02-14 12:37:12 +0700

    feat(core): `CustomPropertyAttribute` to simplify attribute creation

---
+++ UiMetadataFramework.Core/Binding/CustomPropertyAttribute.cs
@@ -0,0 +1,26 @@
+namespace UiMetadataFramework.Core.Binding;
+
+using System;
+
+/// <summary>
+/// <see cref="ICustomPropertyAttribute"/> implementation for complex properties.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
+[CustomPropertyConfig(IsArray = false)]
+public abstract class CustomPropertyAttribute : Attribute, ICustomPropertyAttribute
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CustomPropertyAttribute"/> class.
+	/// </summary>
+	/// <param name="name">Name for the custom property.</param>
+	protected CustomPropertyAttribute(string name)
+	{
+		this.Name = name;
+	}
+
+	/// <inheritdoc />
+	public abstract object GetValue();
+
+	/// <inheritdoc />
+	public string Name { get; }
+}
