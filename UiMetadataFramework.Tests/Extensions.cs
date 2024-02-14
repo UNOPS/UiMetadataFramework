d69b7b6624a066a9eea0d0eeb593d6981ac11935
Nik P <niaher.crimston@gmail.com>
2024-02-14 12:35:44 +0700

    refactor(test): allow any object in `ToDictionary`

--- UiMetadataFramework.Tests/Extensions.cs
+++ UiMetadataFramework.Tests/Extensions.cs
@@ -3,18 +3,18 @@
 	using System.Collections.Generic;
 	using System.Linq;
 	using System.Reflection;
-	using global::MediatR;
 
 	public static class Extensions
 	{
-		public static Dictionary<string, object?> ToDictionary(this IBaseRequest? request)
+		public static Dictionary<string, object?> ToDictionary(this object? request)
 		{
 			if (request == null)
 			{
 				return new Dictionary<string, object?>();
 			}
 
-			return request.GetType().GetProperties()
+			return request.GetType()
+				.GetProperties()
 				.Where(t => t is { CanRead: true, MemberType: MemberTypes.Property })
 				.Where(t => t.GetMethod!.IsPublic)
 				.ToDictionary(t => t.Name, t => t.GetValue(request));
